f1af2d028de99580fe6eb31b5f19ad7c85d00a58
Felipe Flôr <fm6848445@gmail.com>
2026-02-18 08:58:47 -0300

    Teste de commit

---
+++ FinJourney/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinJourney.Entities
+{
+    class Account
+    {
+        private Account()
+        {
+
+
+
+
+
+
+        }
+
+        int Id { get; }
+        string Name { get; }
+        
+        double balance { get; }
+
+        String Type { get; }
+
+      
+
+
+
+
+
+
+    }
+}
